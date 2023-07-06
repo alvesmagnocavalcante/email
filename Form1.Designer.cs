5c6d5cb0276ba64508cc4e03aa01964b22745b05
M4gno4lves <alvesmagnocavalcante@gmail.com>
2023-07-06 12:22:55 -0300

    Enviar emails com C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Banco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxDes = new System.Windows.Forms.TextBox();
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBoxCorpo = new System.Windows.Forms.RichTextBox();
+            this.textBoxAnexo = new System.Windows.Forms.TextBox();
+            this.Anexo = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBoxDes
+            // 
+            this.textBoxDes.Location = new System.Drawing.Point(85, 12);
+            this.textBoxDes.Name = "textBoxDes";
+            this.textBoxDes.Size = new System.Drawing.Size(255, 20);
+            this.textBoxDes.TabIndex = 1;
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(85, 38);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(255, 20);
+            this.textBoxTitulo.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Destinatário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Corpo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Titulo";
+            // 
+            // richTextBoxCorpo
+            // 
+            this.richTextBoxCorpo.Location = new System.Drawing.Point(69, 122);
+            this.richTextBoxCorpo.Name = "richTextBoxCorpo";
+            this.richTextBoxCorpo.Size = new System.Drawing.Size(367, 215);
+            this.richTextBoxCorpo.TabIndex = 7;
+            this.richTextBoxCorpo.Text = "";
+            // 
+            // textBoxAnexo
+            // 
+            this.textBoxAnexo.Location = new System.Drawing.Point(85, 87);
+            this.textBoxAnexo.Name = "textBoxAnexo";
+            this.textBoxAnexo.Size = new System.Drawing.Size(255, 20);
+            this.textBoxAnexo.TabIndex = 8;
+            // 
+            // Anexo
+            // 
+            this.Anexo.AutoSize = true;
+            this.Anexo.Location = new System.Drawing.Point(6, 94);
+            this.Anexo.Name = "Anexo";
+            this.Anexo.Size = new System.Drawing.Size(37, 13);
+            this.Anexo.TabIndex = 9;
+            this.Anexo.Text = "Anexo";
+            this.Anexo.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(82, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Colocar nome do arquivo + .extenção.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(275, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ex: Arquivo.pdf";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 349);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Anexo);
+            this.Controls.Add(this.textBoxAnexo);
+            this.Controls.Add(this.richTextBoxCorpo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTitulo);
+            this.Controls.Add(this.textBoxDes);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "E-mail";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxDes;
+        private System.Windows.Forms.TextBox textBoxTitulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBoxCorpo;
+        private System.Windows.Forms.TextBox textBoxAnexo;
+        private System.Windows.Forms.Label Anexo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace Banco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Configurações do servidor SMTP
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Host = "smtp.office365.com";
+                smtpClient.Port = 587;
+                smtpClient.EnableSsl = true;
+                smtpClient.Credentials = new NetworkCredential("seu e-mail", "sua senha");
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                // Criar a mensagem de e-mail
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress("seu e-mail");
+                message.To.Add(textBoxDes.Text);
+                message.Subject = textBoxTitulo.Text;
+                message.Body = richTextBoxCorpo.Text;
+                message.IsBodyHtml = false;
+
+                //Anexar um arquivo (opcional)
+                Attachment attachment = new Attachment("Caminho da pasta onde esta os arquivos" + textBoxAnexo.Text);
+                message.Attachments.Add(attachment);
+
+
+                // Enviar o e-mail
+                smtpClient.Send(message);
+                MessageBox.Show("E-mail enviado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao enviar o e-mail: " + ex.Message);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
